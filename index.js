9b839c232704361a60ae04030ebbab57cc63013f
adetoadeto <157330742+adetoadeto@users.noreply.github.com>
2024-11-21 19:11:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
 
 if(randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player2 🚩 Wins!"
+    document.querySelector("h1").innerHTML = "Player2 Wins! 🚩"
 } else if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Player1 Wins!"
 } else {
