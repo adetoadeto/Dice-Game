518e09e44931814a31b94aa361031b2738334151
adetoadeto <157330742+adetoadeto@users.noreply.github.com>
2024-11-21 19:01:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
 
-if(randomNumber1 === randomNumber2) {
-    document.querySelector("h1").innerHTML= "Draw!"
+if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player2 🚩 Wins!"
 } else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player1 Wins!"
+    document.querySelector("h1").innerHTML = "🚩 Player1 Wins!"
 } else {
-    document.querySelector("h1").innerHTML="Player2 Wins!"
+  document.querySelector("h1").innerHTML = "Draw!"
 };
